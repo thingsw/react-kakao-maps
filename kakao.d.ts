4a2d25219cd355f8c7874c2e07d6c53c521859e5
Crux <crux153@gmail.com>
2020-03-04 16:04:12 +0900

    Add missing type for Geocoder methods

--- kakao.d.ts
+++ kakao.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for daum-map-api v3
 // Project: http://apis.map.kakao.com/web/documentation/
 // Definitions by: Hyeonsoo David Lee <https://github.com/civilizeddev>
+// Definitions by: Crux <https://github.com/crux153>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Forked from: https://github.com/Musma/daum.maps.d.ts/blob/master/index.d.ts
 
@@ -1842,6 +1843,13 @@ declare namespace kakao.maps.services {
     TM = "TM"
   }
 
+  export enum AddressType {
+    REGION = "REGION",
+    ROAD = "ROAD",
+    REGION_ADDR = "REGION_ADDR",
+    ROAD_ADDR = "ROAD_ADDR"
+  }
+
   /**
    * 주소-좌표간 변환 서비스 객체를 생성한다.
    *
@@ -1849,31 +1857,328 @@ declare namespace kakao.maps.services {
    */
   export class Geocoder {
     /**
-     * 입력한 좌표를 다른 좌표계의 좌표로 변환한다.
+     * 주소 정보에 해당하는 좌표값을 요청한다.
      *
-     * @param x 변환할 x 좌표
-     * @param y 변환할 y 좌표
-     * @param callback 변환 결과를 받을 콜백함수
+     * @param address 변환할 주소명
+     * @param callback 검색 결과를 받을 콜백함수
      * @param options
      */
-    public transCoord(
+    public addressSearch(
+      address: string,
+      callback: (
+        /**
+         * 결과 목록
+         */
+        result: Array<{
+          /**
+           * 전체 지번 주소 또는 전체 도로명 주소. (input에 따라 결정됨)
+           */
+          address_name: string;
+          /**
+           * address_name의 값의 type. 지명, 도로명, 지번주소, 도로명주소 여부
+           */
+          address_type: AddressType;
+          /**
+           * X 좌표값 혹은 longitude
+           */
+          x: string;
+          /**
+           * Y 좌표값 혹은 latitude
+           */
+          y: string;
+          /**
+           * 지번주소 상세 정보
+           */
+          address: {
+            /**
+             * 전체 지번 주소
+             */
+            address_name: string;
+            /**
+             * 지역 1Depth명 - 시도 단위
+             */
+            region_1depth_name: string;
+            /**
+             * 지역 2Depth명 - 구 단위
+             */
+            region_2depth_name: string;
+            /**
+             * 지역 3Depth명 - 동 단위
+             */
+            region_3depth_name: string;
+            /**
+             * 지역 3Depth 행정동 명칭
+             */
+            region_3depth_h_name: string;
+            /**
+             * 행정 코드
+             */
+            h_code: string;
+            /**
+             * 법정 코드
+             */
+            b_code: string;
+            /**
+             * 산 여부
+             */
+            mountain_yn: string;
+            /**
+             * 지번 주 번지
+             */
+            main_address_no: string;
+            /**
+             * 지번 부 번지. 없을 경우 ""
+             */
+            sub_address_no: string;
+            /**
+             * 우편번호 (6자리)
+             */
+            zip_code: string;
+            /**
+             * X 좌표값 혹은 longitude
+             */
+            x: string;
+            /**
+             * Y 좌표값 혹은 latitude
+             */
+            y: string;
+          };
+          /**
+           * 도로명주소 상세 정보
+           */
+          road_address: {
+            /**
+             * 전체 도로명 주소
+             */
+            address_name: string;
+            /**
+             * 지역명1
+             */
+            region_1depth_name: string;
+            /**
+             * 지역명2
+             */
+            region_2depth_name: string;
+            /**
+             * 지역명3
+             */
+            region_3depth_name: string;
+            /**
+             * 도로명
+             */
+            road_name: string;
+            /**
+             * 지하 여부
+             */
+            underground_yn: "Y" | "N";
+            /**
+             * 건물 본번
+             */
+            main_building_no: string;
+            /**
+             * 건물 부번. 없을 경우 ""
+             */
+            sub_building_no: string;
+            /**
+             * 건물 이름
+             */
+            building_name: string;
+            /**
+             * 우편번호(5자리)
+             */
+            zone_no: string;
+            /**
+             * X 좌표값 혹은 longitude
+             */
+            x: string;
+            /**
+             * Y 좌표값 혹은 latitude
+             */
+            y: string;
+          };
+        }>,
+        /**
+         * 응답 코드
+         */
+        status: Status,
+        /**
+         * Pagination 객체
+         */
+        pagination: Pagination
+      ) => void,
+      options?: {
+        /**
+         * 검색할 페이지. 기본값은 1
+         */
+        page: number;
+        /**
+         * 검색할 페이지. 기본값은 10, 1~30 까지 가능
+         */
+        site: number;
+      }
+    ): void;
+
+    /**
+     * 좌표 값에 해당하는 구 주소와 도로명 주소 정보를 요청한다.
+     * 도로명 주소는 좌표에 따라서 표출되지 않을 수 있다.
+     *
+     * @param x x 좌표, 경위도인 경우 longitude
+     * @param y y 좌표, 경위도인 경우 latitude
+     * @param callback 검색 결과를 받을 콜백함수
+     * @param options
+     */
+    public coord2Address(
       x: number,
       y: number,
       callback: (
         /**
-         * 변환된 좌표 결과
+         * 결과 목록
          */
         result: Array<{
-          meta: {
+          /**
+           * 지번주소 상세 정보
+           */
+          address: {
             /**
-             * 매칭된 문서수
+             * 전체 지번 주소
              */
-            total_count: number;
+            address_name: string;
+            /**
+             * 지역 1Depth명 - 시도 단위
+             */
+            region_1depth_name: string;
+            /**
+             * 지역 2Depth명 - 구 단위
+             */
+            region_2depth_name: string;
+            /**
+             * 지역 3Depth명 - 동 단위
+             */
+            region_3depth_name: string;
+            /**
+             * 지번 주 번지
+             */
+            main_address_no: string;
+            /**
+             * 지번 부 번지. 없을 경우 ""
+             */
+            sub_address_no: string;
+            /**
+             * 우편번호 (6자리)
+             */
+            zip_code: string;
           };
-          documents: Array<{
-            x: number;
-            y: number;
-          }>;
+          /**
+           * 도로명주소 상세 정보
+           */
+          road_address: {
+            /**
+             * 전체 도로명 주소
+             */
+            address_name: string;
+            /**
+             * 지역 1Depth명 - 시도 단위
+             */
+            region_1depth_name: string;
+            /**
+             * 지역 2Depth명 - 구 단위
+             */
+            region_2depth_name: string;
+            /**
+             * 지역 3Depth명 - 면 단위
+             */
+            region_3depth_name: string;
+            /**
+             * 도로명
+             */
+            road_name: string;
+            /**
+             * 지하 여부
+             */
+            underground_yn: "Y" | "N";
+            /**
+             * 건물 본번
+             */
+            main_building_no: string;
+            /**
+             * 건물 부번. 없을 경우 ""
+             */
+            sub_building_no: string;
+            /**
+             * 건물 이름
+             */
+            building_name: string;
+            /**
+             * 우편번호(5자리)
+             */
+            zone_no: string;
+          } | null;
+        }>,
+        /**
+         * 응답 코드
+         */
+        status: Status
+      ) => void,
+      options?: {
+        /**
+         * 입력 좌표 체계. 기본값은 WGS84
+         */
+        input_coord?: Coords;
+      }
+    ): void;
+
+    /**
+     * 좌표 값에 해당하는 행정동, 법정동 정보를 얻는다.
+     *
+     * @param x x 좌표, 경위도인 경우 longitude
+     * @param y y 좌표, 경위도인 경우 latitude
+     * @param callback 검색 결과를 받을 콜백함수
+     * @param options
+     */
+    public coord2RegionCode(
+      x: number,
+      y: number,
+      callback: (
+        /**
+         * 결과 목록
+         */
+        result: Array<{
+          /**
+           * 지상/지하/공중 여부 H(행정동) or B(법정동)
+           */
+          region_type: "H" | "B";
+          /**
+           * 전체 지역 명칭
+           */
+          address_name: string;
+          /**
+           * 지역 1Depth명 - 시도 단위(바다지역시 존재안함)
+           */
+          region_1depth_name: string;
+          /**
+           * 지역 2Depth명 - 구 단위(바다지역시 존재안함)
+           */
+          region_2depth_name: string;
+          /**
+           * 지역 3Depth명 - 동 단위(바다지역시 존재안함)
+           */
+          region_3depth_name: string;
+          /**
+           * 지역 4Depth명 - region_type 이 법정동이며, 리 영역인 경우만 존재
+           */
+          region_4depth_name: string;
+          /**
+           * region 코드
+           */
+          code: string;
+          /**
+           * X 좌표값 혹은 longitude
+           */
+          x: number;
+          /**
+           * Y 좌표값 혹은 latitude
+           */
+          y: number;
         }>,
         /**
          * 응답 코드
@@ -1890,6 +2195,43 @@ declare namespace kakao.maps.services {
          * 출력 좌표 체계. 기본값은 WGS84
          */
         output_coord?: Coords;
+      }
+    ): void;
+
+    /**
+     * 입력한 좌표를 다른 좌표계의 좌표로 변환한다.
+     *
+     * @param x 변환할 x 좌표
+     * @param y 변환할 y 좌표
+     * @param callback 변환 결과를 받을 콜백함수
+     * @param options
+     */
+    public transCoord(
+      x: number,
+      y: number,
+      callback: (
+        /**
+         * 변환된 좌표 결과
+         */
+        result: Array<{
+          x: number;
+          y: number;
+        }>,
+        /**
+         * 응답 코드
+         */
+        status: Status
+      ) => void,
+      options: {
+        /**
+         * 입력 좌표 체계. 기본값은 WGS84
+         */
+        input_coord?: Coords;
+
+        /**
+         * 출력 좌표 체계. 기본값은 WGS84
+         */
+        output_coord: Coords;
       }
     ): void;
   }
